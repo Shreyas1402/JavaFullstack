b0c21fab1bab972c62af3bde6600e9e88f26cf91
Shreyas <shreyaspappu.201ee253@nitk.edu.in>
2022-06-08 16:11:28 +0530

    Mathe

---
+++ src/MathematicalOperators.java
@@ -0,0 +1,30 @@
+
+
+public class MathematicalOperators {
+
+    public static void main(String[] args) {
+        int x = 52;
+        int y = 85;
+        int result;
+
+
+        //ADDITION
+        result = x + y;
+        System.out.println("addition is " + result);
+
+        // SUBTRACTION
+        result = y - x;
+        System.out.println("Subtraction is " + result);
+
+        //MULTIPLICATION
+        result = y * x;
+
+        System.out.println("Multiplication is " + result);
+
+        //DIVISION
+        result = y / x;
+        System.out.println("division is " + result);
+    }
+
+
+}
